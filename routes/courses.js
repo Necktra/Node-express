f50db62a08e26a1364025af54c7dd856e8aefbf5
Irina <hallo.ween@mail.ru>
2022-01-13 18:58:09 +0300

    course model for mongoose update for rest file

--- routes/courses.js
+++ routes/courses.js
@@ -1,9 +1,11 @@
-const {Router} = require('express');
+const {
+    Router
+} = require('express');
 const Course = require('../models/course');
 const router = Router();
 
 router.get('/', async (req, res) => {
-    const courses = await Course.getAll();
+    const courses = await Course.find().lean();
     res.render('courses', {
         title: 'Курсы',
         isCourses: true,
@@ -12,10 +14,11 @@ router.get('/', async (req, res) => {
 });
 
 router.get('/:id/edit', async (req, res) => {
+    //console.log(12121);
     if (!req.query.allow) {
         return res.redirect('/')
     }
-    const course = await Course.getById(req.params.id);
+    const course = await Course.findById(req.params.id).lean();
     res.render('course-edit', {
         title: `Редактировать ${course.title}`,
         course
@@ -23,12 +26,16 @@ router.get('/:id/edit', async (req, res) => {
 });
 
 router.post('/edit', async (req, res) => {
-    await Course.update(req.body);
+    const {
+        id
+    } = req.body;
+    delete req.body.id;
+    await Course.findByIdAndUpdate(id, req.body).lean();
     res.redirect('/courses');
 });
 
 router.get('/:id', async (req, res) => {
-    const course = await Course.getById(req.params.id);
+    const course = await Course.findById(req.params.id).lean();
     res.render('course', {
         layout: 'empty',
         title: `Курс ${course.title}`,
