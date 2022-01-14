491cf11dcfdd9c0ecd7eb05498cf58a093d69d13
Irina <hallo.ween@mail.ru>
2022-01-14 11:03:18 +0300

    cart view update

--- routes/card.js
+++ routes/card.js
@@ -4,6 +4,18 @@ const {
 const Course = require('../models/course');
 const router = Router();
 
+function mapCartItems(cart) {
+    return cart.items.map(c => ({
+        ...c.courseId._doc, count: c.count
+    }))
+}
+
+function computePrice(courses) {
+    return courses.reduce((total, course) => {
+        return total += course.price * course.count
+    }, 0)
+} 
+
 router.post('/add', async (req, res) => {
     const course = await Course.findById(req.body.id).lean();
     await req.user.addToCart(course);
@@ -16,12 +28,13 @@ router.delete('/remove/:id', async (req, res) => {
 });
 
 router.get('/', async (req, res) => {
-    const card = await Card.fetch();
+    const user = await req.user.populate('cart.items.courseId');
+    const courses = mapCartItems(user.cart);
     res.render('card', {
         title: 'Корзина',
         isCard: true,
-        courses: card.courses,
-        price: card.price
+        courses: courses,
+        price: computePrice(courses)
     })
 });
 
