1f7ce8b7d32e4ceff4996b542cbeb3ee97dc1f5d
Irina <hallo.ween@mail.ru>
2022-01-15 12:28:33 +0300

    add auth check for nav

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,6 @@
 const {
     Router
 } = require('express');
-// const Course = require('../models/course');
 const router = Router();
 
 router.get('/login', async (req, res) => {
@@ -9,6 +8,17 @@ router.get('/login', async (req, res) => {
         title: 'Авторизация',
         isLogin: true
     });
+});
+
+router.get('/logout', async (req, res) => {
+    req.session.destroy(() => {
+        res.redirect('/auth/login#login');
+    });
+});
+
+router.post('/login', async (req, res) => {
+    req.session.isAuthenticated = true;
+    res.redirect('/');
 });
 
 module.exports = router;
