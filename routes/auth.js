a7d30d424f1d06cc2c4f8f265105f590aa388377
Irina <hallo.ween@mail.ru>
2022-01-15 17:22:24 +0300

    user login update

--- routes/auth.js
+++ routes/auth.js
@@ -18,15 +18,35 @@ router.get('/logout', async (req, res) => {
 });
 
 router.post('/login', async (req, res) => {
-    const user = await User.findById('61e06587c181f8df0bc64b24');
-    req.session.user = user;
-    req.session.isAuthenticated = true;
-    req.session.save(err => {
-        if (err) {
-            throw err
-        };
-        res.redirect('/');
-    })
+    try {
+        const {
+            email,
+            password
+        } = req.body;
+        const candidate = await User.findOne({
+            email
+        });
+
+        if (candidate) {
+            const areSame = password === candidate.password;
+            if (areSame) {
+                req.session.user = candidate;
+                req.session.isAuthenticated = true;
+                req.session.save(err => {
+                    if (err) {
+                        throw err
+                    };
+                    res.redirect('/');
+                })
+            } else {
+                res.redirect('/auth/login#login');
+            }
+        } else {
+            res.redirect('/auth/login#login');
+        }
+    } catch (e) {
+        console.log(e);
+    }
 });
 
 router.post('/register', async (req, res) => {
